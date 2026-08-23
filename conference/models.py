fcbc60f3c640b7323eb1bc48bdcf3556354ffb15
Carter Turnbaugh <carterturn@tutanota.de>
2026-08-23 11:48:58 -0400

    Cleanup program page a bit.

--- conference/models.py
+++ conference/models.py
@@ -116,6 +116,10 @@ class ProgramEvent(models.Model):
     date = models.DateField()
     start_time = models.TimeField(blank=False)
     end_time = models.TimeField(blank=True, null=True)
+    description = models.CharField(max_length=2048, blank=True)
+
+    def __str__(self):
+        return 'QuARC %d %s' % (self.quarc.year, self.event)
 
     class Meta:
         verbose_name = 'QuARC Event'
