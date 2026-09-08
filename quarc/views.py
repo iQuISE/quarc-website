477f0b5083ba3dfceeb42e7f861e65fd35d9f393
Carter Turnbaugh <carterturn@tutanota.de>
2026-09-08 11:02:35 -0400

    More robust default page

--- quarc/views.py
+++ quarc/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from datetime import datetime
+
 from conference.models import QuARCConference
 
 def index(request):
@@ -10,6 +12,10 @@ def index(request):
     most_recent_quarc = QuARCConference.objects.order_by('year').last()
     if most_recent_quarc is not None:
         return redirect(reverse('conference_home', args=[most_recent_quarc.year]))
+    year = datetime.now().year
+    if datetime.now().month > 2:
+        year += 1
+    return redirect(reverse('conference_home', args=[str(year)]))
 
 def perm_redirect(request, url):
     return redirect(url, permanent=False, preserve_request=False)
