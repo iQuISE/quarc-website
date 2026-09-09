b9a29efc021d8bda97a2836e93dad863cdfc2773
Carter Turn <carterturn@tutanota.de>
2026-09-09 17:36:31 -0400

    Add trailing / to API urls

--- conference/urls.py
+++ conference/urls.py
@@ -13,11 +13,11 @@ urlpatterns = [
     path('quarc<int:year>/university-and-industry-personnel/', views.registration_university_industry, name='registration_university_industry'),
     path('quarc<int:year>/abstract-submission-closed/', views.registration_closed, name='registration_closed'),
     # API
-    path('api<int:year>/attendees', api.export_attendees, name='api_export_attendees'),
-    path('api<int:year>/housing_preferences', api.export_housing_preferences, name='api_export_housing_preferences'),
-    path('api<int:year>/dinner', api.export_dinner, name='api_export_dinner'),
-    path('api<int:year>/activities', api.export_activities, name='api_export_activities'),
-    path('api<int:year>/swag', api.export_swag, name='api_export_swag'),
-    path('api<int:year>/bus', api.export_bus, name='api_export_bus'),
-    path('api<int:year>/housing_assignments', api.export_housing_assignments, name='api_export_housing_assignments'),
+    path('api<int:year>/attendees/', api.export_attendees, name='api_export_attendees'),
+    path('api<int:year>/housing_preferences/', api.export_housing_preferences, name='api_export_housing_preferences'),
+    path('api<int:year>/dinner/', api.export_dinner, name='api_export_dinner'),
+    path('api<int:year>/activities/', api.export_activities, name='api_export_activities'),
+    path('api<int:year>/swag/', api.export_swag, name='api_export_swag'),
+    path('api<int:year>/bus/', api.export_bus, name='api_export_bus'),
+    path('api<int:year>/housing_assignments/', api.export_housing_assignments, name='api_export_housing_assignments'),
 ]
